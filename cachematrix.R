a13abe85909c2e9de7a407213758883d2a0b9455
mahimagupta29 <43781052+mahimagupta29@users.noreply.github.com>
2020-04-14 23:11:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -37,7 +37,7 @@ cacheSolve <- function(x, ...) {
   x$setinv(inv)
   inv
 }
-## ---------------Checking the program------------------------
+## ---------------Checking the program-----------------
 ## m <- matrix(rnorm(16),4,4)
 ## m1 <- makeCacheMatrix(m)
 ## cacheSolve(m1)
